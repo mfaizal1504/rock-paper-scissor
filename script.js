9981d4ddb925a08816bca8c2510288b2f602eb0b
faizal <128202217+mfaizal1504@users.noreply.github.com>
2025-02-13 23:00:49 +0800

    Add pseudocode for a single round run

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 console.log("Hello there!")
+
+//Steps for 1 round
+//1. Create bot_choice to store choice made by bot
+//2. Create player_choice to store choice made by player
+//3. Create bot_score to store score for bot
+//4. Create player_score to keep score for player
+//5. Create random_value with a random value
+//6. Set value of bot_choice based on the value of random_value (0-33=rock, 34-66=paper, 67-100=scissor)
+//7. Set value for player_choice by asking input using prompt
